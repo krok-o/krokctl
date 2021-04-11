db36ebfb8531721f16fdb8cd591e0155dd31805b
Brautigam Gergely <skarlso777@gmail.com>
2021-04-11 21:49:32 +0200

    Creating the login command which save the session in a file

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"io/ioutil"
 	"os"
+	"path/filepath"
 
 	"github.com/krok-o/krokctl/pkg"
 	"github.com/rs/zerolog"
@@ -11,6 +13,7 @@ import (
 const (
 	// Prefix of all environment variables
 	envKeyPrefix = "KROK_"
+	sessionFile  = ".session"
 )
 
 var (
@@ -34,6 +37,9 @@ var (
 	}
 	// KC is the krok client with all the clients bundled together.
 	KC *pkg.KrokClient
+
+	// sessionFileLocation
+	sessionFileLocation string
 )
 
 func getEnvOrDefault(key, def string) string {
@@ -55,6 +61,18 @@ func init() {
 	KC = pkg.NewKrokClient(pkg.Config{
 		Address: KrokArgs.endpoint,
 	}, CLILog)
+
+	// Set up session file location
+	home, err := os.UserHomeDir()
+	if err != nil {
+		CLILog.Fatal().Err(err).Msg("Failed to get user home directory.")
+	}
+	sessionFileLocation = filepath.Join(home, ".config", "krokctl")
+	if _, err := os.Stat(sessionFileLocation); err != nil && os.IsNotExist(err) {
+		if err := os.MkdirAll(sessionFileLocation, os.ModeDir); err != nil {
+			CLILog.Fatal().Err(err).Msg("Failed to create krokctl config folder.")
+		}
+	}
 }
 
 // ShowUsage shows usage of the given command on stdout.
@@ -70,6 +88,22 @@ func Execute() error {
 // set up a token before the actual command is executed.
 func krokCmdPersistentPreRun(cmd *cobra.Command, args []string) {
 	if KrokArgs.Token == "" {
-		KrokArgs.Token = getEnvOrDefault("TOKEN", "")
+		var err error
+		KrokArgs.Token, err = getTokenFromSessionFile()
+		if err != nil {
+			CLILog.Fatal().Err(err).Msg("Failed to locate session file. Please log in.")
+		}
 	}
+}
+
+func getTokenFromSessionFile() (string, error) {
+	if _, err := os.Stat(filepath.Join(sessionFileLocation, sessionFile)); err != nil {
+		return "", err
+	}
+
+	content, err := ioutil.ReadFile(filepath.Join(sessionFileLocation, sessionFile))
+	if err != nil {
+		return "", err
+	}
+	return string(content), nil
 }
