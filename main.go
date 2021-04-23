26daeba5f998cd693231745b7a9a6bc4245160e1
Brautigam Gergely <skarlso777@gmail.com>
2021-04-23 09:59:02 +0200

    Added events command

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/krok-o/krokctl/cmd"
 	_ "github.com/krok-o/krokctl/cmd/commands"
+	_ "github.com/krok-o/krokctl/cmd/events"
 	_ "github.com/krok-o/krokctl/cmd/platforms"
 	_ "github.com/krok-o/krokctl/cmd/repositories"
 	_ "github.com/krok-o/krokctl/cmd/settings"
